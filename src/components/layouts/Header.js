5828bbe5a2ab32db26bb5945f0f261d9993727fd
portafolio <edgar.cortes1812@gmail.com>
2023-06-14 11:14:08 -0600

    Se removieron algunas clases para fijar la navbar
    Se movieron unos estilos para fijar la navbar.

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -23,33 +23,16 @@ class Header extends HeaderComponent {
                     </div>
                     <Mobilemenu />
                 </aside>
-                <div className="aside-overlay aside-trigger" onClick={this.navtoggleClass} />
                 {/* Header Start */}
-                <header className={`main-header header-fw can-sticky header-1 ${stickyheader}`}>
-                    {/* Top Header Start */}
-                    <div className="top-header">
-                        <div className="top-header-inner">
-                            <ul className="social-media">
-                                <li> <Link to="#"> <i className="fab fa-facebook-f" /> </Link> </li>
-                                <li> <Link to="#"> <i className="fab fa-pinterest-p" /> </Link> </li>
-                                <li> <Link to="#"> <i className="fab fa-linkedin-in" /> </Link> </li>
-                                <li> <Link to="#"> <i className="fab fa-twitter" /> </Link> </li>
-                            </ul>
-                            <ul className="top-header-nav">
-                                <li> <Link to="/login"> Login</Link> </li>
-                                <li>or</li>
-                                <li> <Link to="/register"> Signup</Link> </li>
-                            </ul>
-                        </div>
-                    </div>
-                    {/* Top Header End */}
+                <header className={"main-header header-fw can-sticky header-1 sticky"}>
+
                     <nav className="navbar">
                         {/* Menu */}
                         <Menu />
                         <div className="header-controls">
                             <ul className="header-controls-inner d-none d-lg-flex">
                                 <li>
-                                    <Link to="/submit-listing" className="btn-custom primary">Submit Listing <i className="fas fa-plus" /> </Link>
+                                    <Link to="/login"> Login</Link> 
                                 </li>
                             </ul>
                             {/* Toggler */}
